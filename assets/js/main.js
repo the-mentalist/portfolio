760adc5de6739d56a56fd83d050f1fd18c12b9ce
Navpreet Ahuja <Navpreet Ahuja>
2017-05-16 20:51:23 +0530

    style moved to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+	$('#content-box1').css('display','block');
+	$('#content-box2').css('display','none');
+	$('#content-box3').css('display','none');
+});
+
 function showContent(id){
 	for(var i=1; i<=3; i++)
 		$('#content-box'+i).css('display','none');
